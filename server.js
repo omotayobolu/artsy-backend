aca412dca51f906cb8891e115b20551600b01f52
omotayobolu <omotayodaniel10@gmail.com>
2025-04-11 18:19:31 +0100

    fix: change to domain url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const notFoundMiddleware = require("./middlewares/not-found");
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://artsyphotography.vercel.app/",
+  "https://artsyphotography.vercel.app",
 ];
 
 app.use(
